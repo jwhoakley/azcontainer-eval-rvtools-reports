3627539ad5b786e7abaef4a35e2726a77a871915
jwhoakley <william@jwho.co.uk>
2024-06-10 17:47:33 +0100

    Initial setup and base files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # azcontainer-eval-rvtools-reports
 Azure container to evaluate rvtools reports
+
+c.f. Based on the MS Learn page for creating containers in Azure with Terraform - https://learn.microsoft.com/en-us/azure/container-instances/container-instances-quickstart-terraform
+
---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  cloud {
+    organization = "AZJWHO"
+    workspaces {
+      name = "terraform-azure-container-test-1"
+    }
+  }
+
+}
+provider "azurerm" {
+  features {}
+}
